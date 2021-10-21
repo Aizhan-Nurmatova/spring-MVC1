9838dcd3b6975bbff61d5fb65b853645518696cf
nitro5 <AizhanFromPekasoft>
2021-10-21 14:15:51 +0300

    Initial commit " "

---
+++ src/main/java/com/peaksoft/config/WebConfig2.java
@@ -0,0 +1,18 @@
+package com.peaksoft.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.peaksoft")
+public class WebConfig2 {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        return new InternalResourceViewResolver("/WEB-INF/views/", ".html");
+    }
+}
